539bacd0d53ee8614426f16d14bfefdb3172fb0c
@thiankiann <thiankiann@gmail.com>
2023-09-03 11:33:35 +0100

    Init Commit

---
+++ src/main/java/com/example/resttemplate/ShawnMendesProxy.java
@@ -0,0 +1,14 @@
+package com.example.resttemplate;
+import org.springframework.web.client.RestTemplate;
+
+// GET https://itunes.apple.com/search?term=shawnmendes&limit=1
+
+public class ShawnMendesProxy {
+
+    RestTemplate restTemplate;
+
+    public void makeShawnMendesRequest(String term, Integer limit){
+
+    }
+
+}
