5fb6853ede74d50f38c9c7057f17b41e7db09a08
@thiankiann <thiankiann@gmail.com>
2023-09-03 11:51:41 +0100

    @EventListener

--- src/main/java/com/example/resttemplate/ShawnMendesProxy.java
+++ src/main/java/com/example/resttemplate/ShawnMendesProxy.java
@@ -1,13 +1,15 @@
 package com.example.resttemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 // GET https://itunes.apple.com/search?term=shawnmendes&limit=1
 
+@Component
 public class ShawnMendesProxy {
 
     RestTemplate restTemplate;
 
-    public void makeShawnMendesRequest(String term, Integer limit){
+    public String makeShawnMendesRequest(String term, Integer limit){
 
     }
 
