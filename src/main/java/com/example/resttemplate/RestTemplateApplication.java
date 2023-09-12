f802fe9fa818253b79e0883ec09ae80034305ce6
@thiankiann <thiankiann@gmail.com>
2023-09-12 07:24:45 +0100

    spell

--- src/main/java/com/example/resttemplate/RestTemplateApplication.java
+++ src/main/java/com/example/resttemplate/RestTemplateApplication.java
@@ -1,6 +1,7 @@
 package com.example.resttemplate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
